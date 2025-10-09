974dfe377b54c1d8706fa766547e82bbaa9dc3d2
seo000408@gmail.com <seo000408@gmail.com>
2025-10-10 05:01:01 +0900

    making blog template

--- my-first-blog/mysite/urls.py
+++ my-first-blog/mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+
+"""mysite URL Configuration[...]"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    re_path(r'^$', include('blog.urls')),
 ]
